c6e99456a57aceaabcd5340e074398858730acfc
Kevin Liu <greatplateau@hotmail.com>
2015-06-08 17:10:30 +1000

    adding category completed

--- api/jobooking/jobs/serializer.py
+++ api/jobooking/jobs/serializer.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from .models import Job
- 
+from .models import Category
+
  
 class JobSerializer(serializers.ModelSerializer):
     class Meta:
         model = Job
         fields = ('id', 'job_reference','job_title','job_cost' )
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('category_name',)
